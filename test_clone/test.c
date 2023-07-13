113be7c4cbd415428beb3e87fe246c38d5620ff6
Maor Kleinberger <kmaork@gmail.com>
2023-07-13 20:24:15 +0300

    Minimize POC

--- test_clone/test.c
+++ test_clone/test.c
@@ -6,8 +6,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define STACK_SIZE (1024 * 1024)    /* Stack size for cloned child */
-
+#define STACK_SIZE (1024 * 4)
 
 int threadFunction(void *arg) {
     void* handle = dlopen("./loaded.so", RTLD_LAZY);
@@ -16,25 +15,10 @@ int threadFunction(void *arg) {
 }
 
 int main() {
-    char *stack;
-    char *stackTop;
-    int status;
-
-    /* Allocate stack for child */
-    stack = malloc(STACK_SIZE);
-    if (stack == NULL) {
-        perror("malloc");
-        exit(1);
-    }
-
-    /* Assume stack grows downward */
-    stackTop = stack + STACK_SIZE;
-
-    if (clone(threadFunction, stackTop, CLONE_VM, 0) == -1) {
+    if (clone(threadFunction, malloc(STACK_SIZE) + STACK_SIZE, CLONE_VM, 0) == -1) {
         perror("clone");
         exit(1);
     }
-
     usleep(10000);
     return 0;
 }
